e981e21c937cad42a2421f0fd94a1f3cc2520f2f
TheGregAllison <146021687+TheGregAllison@users.noreply.github.com>
2023-10-04 12:14:12 -0500

    Added Pokemon List and Notes
    Snorlax added to our pokemon list, with for loop indicating if a pokemon is larger than 1m.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,16 @@
 let pokemonList = [
     {name: "Pikachu", type: "Electric", height: .4},
     {name: "Metapod", type: "Bug", height: .7},
-    {name: "Magikarp", type: "Water", height: .9}
+    {name: "Magikarp", type: "Water", height: .9},
+    {name: "Snorlax", type: "Normal", height: 2.1}
 ];
+//This for loop runs through the array and displays the Pokemon, their height and a special note about them in the DOM. 
+for (let i = 0; i < pokemonList.length; i++){
+    document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + " m) " )
+    // This if statement adds a note in the DOM about the pokemon's size. 
+    if (pokemonList[i].height >= 1){
+        document.write("- Now that's a large Pokemon.")
+    }
+    document.write("<br>")
+}
+
