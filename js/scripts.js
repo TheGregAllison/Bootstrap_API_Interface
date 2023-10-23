660ba447ffc3b7c9b85091cef1be46c63b6aadbe
TheGregAllison <146021687+TheGregAllison@users.noreply.github.com>
2023-10-23 13:34:08 -0500

    Repository Pulls Pokemon API
    This update pulls the Pokemon list from the Pokemon API and displays the logs the specific Pokemon's details to the console when clicked.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,8 @@
 let pokemonRepository = (function () {
 
-    //Pokemon repository array with objects for example pokemon
-    let pokemonList = [
-        { name: "Pikachu", type: "Electric", height: .4 },
-        { name: "Metapod", type: "Bug", height: .7 },
-        { name: "Magikarp", type: "Water", height: .9 },
-        { name: "Snorlax", type: "Normal", height: 2.1 }
-    ];
+    //Empty Array, assigns API URL with pokemon API. 
+    let pokemonList = [];
+    let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
     //Function that simply returns the above array pokemonList
     function getAll() {
@@ -26,7 +22,7 @@ let pokemonRepository = (function () {
     }
 
     // This function creates li and button elements within the pokemon-list class on the index.html page. Additionally, this includes a local function that logs the pokemon's name when its corresponding button is clicked. 
-    function addListItem(pokemon){
+    function addListItem(pokemon) {
         let pokemonList = document.querySelector(".pokemon-list");
         let pokemonItem = document.createElement("li");
         let pokemonButton = document.createElement("button");
@@ -34,20 +30,63 @@ let pokemonRepository = (function () {
         pokemonButton.classList.add("button-class");
         pokemonItem.appendChild(pokemonButton);
         pokemonList.appendChild(pokemonItem);
-        pokemonButton.addEventListener("click", function showDetails(){
-            console.log(pokemon.name)
-        });   
+        pokemonButton.addEventListener("click", function () {
+            showDetails(pokemon)
+        })
     }
-    
+
+    function showDetails(pokemon) {
+        console.log(pokemon.name)
+    }
+
+    function loadList() {
+        return fetch(apiUrl).then(function (response) {
+            return response.json();
+        }).then(function (json) {
+            json.results.forEach(function (item) {
+                let pokemon = {
+                    name: item.name,
+                    detailsUrl: item.url
+                };
+                addListItem(pokemon);
+            });
+        }).catch(function (e) {
+            console.error(e)
+        })
+    }
+
+    function loadDetails(item) {
+        let url = item.detailsUrl;
+        return fetch(url).then(function (response)
+         {
+            return response.json();
+         }).then(function (details) {
+            item.imageUrl = details.sprites.front_default;
+            item.height = details.height;
+            item.types = details.types;
+         }).catch(function (e){
+            console.error(e);
+         });
+    }
+
+    function showDetails(pokemon) {
+        loadDetails(pokemon).then(function (){
+            console.log(pokemon);
+        });
+    }
+
     //pokemonRepository function return values
     return {
-        getAll: getAll,
         add: add,
-        addListItem: addListItem
+        getAll: getAll,
+        loadList: loadList,
+        loadDetails: loadDetails
     };
 })();
 
 
-pokemonRepository.getAll().forEach(function(pokemon){
-pokemonRepository.addListItem(pokemon);
+pokemonRepository.loadList().then(function () {
+    pokemonRepository.getAll().forEach(function (pokemon) {
+        pokemonRepository.addListItem(pokemon);
+    });
 });
